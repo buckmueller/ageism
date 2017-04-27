690eecd5b970735ba61a2648fbae78301fde10c1
buckmueller <buckmueller69@gmail.com>
2017-04-26 21:46:58 -0500

    finished form validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,28 +1,28 @@
 window.onload = function () {
     var form = document.getElementById("contactForm");
     form.onsubmit = function (e) {
-        var first = document.getElementById("first");
-        var last = document.getElementById("last");
-        var email = document.getElementById("email");
-        
-        var nameReg = /[a-zA-Z]+/g;
-        var emailReg = /[a-zA-Z0-9._]+@[a-zA-Z]+.[a-zA-Z]{3}/;
-        
-        if (!nameReg.test(first)) {
-            //alert("First name must be filled out ");
-            alert("First name must contain no special characters");
-            return false;
-        } 
-        if (!nameReg.test(last)) {
-            //alert("First name must be filled out ");
-            alert("Last name must contain no special characters");
-            return false;
+        var first = document.getElementById("first").value, last = document.getElementById("last").value, email = document.getElementById("email").value, nameReg = /[a-zA-Z]+/, emailReg = /[a-zA-Z0-9._]+@[a-zA-Z]+.[a-zA-Z]{3}/, flag = false, output = "";
+        console.log(first);
+        if (!nameReg.test(first) || first === "") {
+            output += "First name must contain no special characters\n";
+            flag = true;
         }
+        console.log(last);
+        if (!nameReg.test(last) || last === "") {
+            output += "Last name must contain no special characters\n";
+            flag = true;
+        }
+        console.log(email);
         if (!emailReg.test(email)) {
-            //alert("First name must be filled out ");
-            alert("Invalid email");
+            output += "Invalid email\n";
+            flag = true;
+        }
+        
+        if (flag) {
+            console.log(output);
+            alert(output);
             return false;
         }
-    }
+    };
     
-}
+};
