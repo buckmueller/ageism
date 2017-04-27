185ccda9a8e775b1a5ba8baf2f7d54b5dfa9501f
buckmueller <buckmueller69@gmail.com>
2017-04-26 21:26:33 -0500

    validator

--- scripts/validate.js
+++ scripts/validate.js
@@ -5,13 +5,24 @@ window.onload = function () {
         var last = document.getElementById("last");
         var email = document.getElementById("email");
         
-        var nameReg = /[a-zA-Z]/g;
+        var nameReg = /[a-zA-Z]+/g;
+        var emailReg = /[a-zA-Z0-9._]+@[a-zA-Z]+.[a-zA-Z]{3}/;
         
         if (!nameReg.test(first)) {
             //alert("First name must be filled out ");
-            alert("No special characters");
+            alert("First name must contain no special characters");
             return false;
-        }   
+        } 
+        if (!nameReg.test(last)) {
+            //alert("First name must be filled out ");
+            alert("Last name must contain no special characters");
+            return false;
+        }
+        if (!emailReg.test(email)) {
+            //alert("First name must be filled out ");
+            alert("Invalid email");
+            return false;
+        }
     }
     
 }
